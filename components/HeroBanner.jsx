6e45d5bff82b93094edb4331ebc6f85738c1a22b
Tobias He <tobiashe17@gmail.com>
2022-07-12 15:12:27 -0400

    hooking up with sanity
    displaying images from sanity database

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
 
-const HeroBanner = () => {
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({heroBanner}) => {
   return (
     <div className="hero-banner-container">
-        <p className="beats-solo">text holder</p>
-        <h3>mid text</h3>
-        <img src="" alt="headphones"
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones"
         className="hero-banner-image"/>
 
         <div>
